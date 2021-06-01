0fa538fe6459c540534512d2ed44b532433c98b3
Paul Cowan <paul.cowan@cutting.scot>
2021-06-01 11:12:06 +0100

    add login fail test

--- Sample-01/cypress/integration/login.ts
+++ Sample-01/cypress/integration/login.ts
@@ -1,45 +1,66 @@
 import type { Client, Scenario, Simulation } from '@simulacrum/client';
 import { createClient } from '@simulacrum/client'
 
+interface Person { email: string; password: string }
+
 describe('login', () => {
   let client: Client;
   let simulation: Simulation;
-  let user: { email: string };
+  let person: Person;
     
   before(async () => {
     client = createClient('http://localhost:4000');
     simulation = await client.createSimulation("auth0");
-    let scenario = await client.given(simulation, "person") as Scenario<{email: string}>;
-    user = scenario.data;
+    let scenario = await client.given(simulation, "person") as Scenario<Person>;
+    person = scenario.data;
   });
 
   after(() => {
-    client.destroySimulation(simulation);
+    // client.destroySimulation(simulation);
   })
-  
-  it('login', () => {
+
+  it('should fail login', () => {
     cy.visit('/')
 
     cy.get('#qsLoginBtn').first().click();
 
     cy.get('#username')
-    .type(user.email)
-    .should('have.value', user.email);
+    .type('bob@gmail.com');
 
     cy.get('#password')
-    .type('123$567')
-    .should('have.value', '123$567');
+    .type('sdfsdfsdfsd');
 
     cy.get('#submit').click();
 
-    cy.get('.nav-link').contains('External API')
+    cy.get('.nav-link').should('not.exist');
   })
   
-  it('should access restricted route',  () => {
-    cy.login({currentUser: user.email});
+  it('should login', () => {
+    cy.visit('/')
+
+    cy.get('#qsLoginBtn').first().click();
+
+    cy.get('#username')
+    .type(person.email)
+    .should('have.value', person.email);
+
+    cy.get('#password')
+    .type(person.password)
+    .should('have.value', person.password);
+
+    cy.get('#submit').click();
+
+    cy.get('.nav-link').should('contain', 'External API');
+  })
+  
+  it('should get token without logging in and access restricted route',  async () => {
+    let { data } = await client.given(simulation, "person") as Scenario<Person>;
+    let person2 =  data;
+    
+    cy.login({currentUser: person2.email});
 
     cy.visit('/external-api');
 
-    cy.get('h1').contains('External')
+    cy.url().should('include', '/external-api');
   });
 });
