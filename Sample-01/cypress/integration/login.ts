a4d514bc9be78497a23285c35629dbb48ed00204
Paul Cowan <paul.cowan@cutting.scot>
2021-06-02 10:45:17 +0100

    rename tests

--- Sample-01/cypress/integration/login.ts
+++ Sample-01/cypress/integration/login.ts
@@ -12,58 +12,58 @@ describe('login', () => {
     client = createClient('http://localhost:4000');
   });
 
-  // afterEach(() => {
-  //   client.destroySimulation(simulation);
-  // })
-  
   beforeEach(async () => {
     simulation = await client.createSimulation("auth0");
     let scenario = await client.given(simulation, "person") as Scenario<Person>;
     person = scenario.data;
-  })
-
-  it('should fail login', () => {
-    cy.visit('/')
-
-    cy.get('#qsLoginBtn').first().click();
-
-    cy.get('#username')
-    .type('bob@gmail.com');
-
-    cy.get('#password')
-    .type('sdfsdfsdfsd');
-
-    cy.get('#submit').click();
-
-    cy.get('.nav-link').should('not.exist');
-  })
-  
-  it('should login', () => {
-    cy.visit('/')
-
-    cy.get('#qsLoginBtn').first().click();
-
-    cy.get('#username')
-    .type(person.email)
-    .should('have.value', person.email);
-
-    cy.get('#password')
-    .type(person.password)
-    .should('have.value', person.password);
-
-    cy.get('#submit').click();
-
-    cy.get('.nav-link').should('contain', 'External API');
-  })
-  
-  it('should get token without logging in and access restricted route',  async () => {
-    let { data } = await client.given(simulation, "person") as Scenario<Person>;
-    let person2 =  data;
-    
-    cy.login({currentUser: person2.email});
-
-    cy.visit('/external-api');
-
-    cy.url().should('include', '/external-api');
   });
+ 
+  describe('Universal Login', () => {
+    it('should fail login', () => {
+      cy.visit('/')
+  
+      cy.get('#qsLoginBtn').first().click();
+  
+      cy.get('#username')
+      .type('bob@gmail.com');
+  
+      cy.get('#password')
+      .type('sdfsdfsdfsd');
+  
+      cy.get('#submit').click();
+  
+      cy.get('.nav-link').should('not.exist');
+    })
+    
+    it('should login', () => {
+      cy.visit('/')
+  
+      cy.get('#qsLoginBtn').first().click();
+  
+      cy.get('#username')
+      .type(person.email)
+      .should('have.value', person.email);
+  
+      cy.get('#password')
+      .type(person.password)
+      .should('have.value', person.password);
+  
+      cy.get('#submit').click();
+  
+      cy.get('.nav-link').should('contain', 'External API');
+    })
+  })
+
+  describe('normal flow', () => {
+    it('should get token without signing in and access restricted route',  async () => {
+      let { data } = await client.given(simulation, "person") as Scenario<Person>;
+      let person2 =  data;
+      
+      cy.login({currentUser: person2.email});
+  
+      cy.visit('/external-api');
+  
+      cy.url().should('include', '/external-api');
+    });
+  })
 });
